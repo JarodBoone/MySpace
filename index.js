8aaa928f3b859f5312d706818275b65a5bc8f882
JarodBoone <jtboone12@gmail.com>
2018-01-27 20:27:11 -0500

    changed server code and cleaned repo

--- index.js
+++ index.js
@@ -1,42 +1,43 @@
-var express = require('express');
+// Hack@Brown 2018 WebAR project -- Network Code 
+// Jarod Boone, Catherine Habgood, Duncan McManus 
+
+// Modules 
 var bp = require('body-parser');
 var path = require('path');
 var ejs = require('ejs');
+var http = require('http'); 
+var fs = require('fs'); // file system io 
 
-//instantiate express instance
-var app = express();
+var a = require('./httpServer'); 
 
 //if the port is an env var, take it, else default
-const port = process.env.port || 3012;
+const port = process.env.port || 8080;
 const backup_port = process.env.port || 3011;
 
-// ******** SET THIS UP LATER *************
-// //set up even listeners on database get
-// var db = mongoose.connection;
-// db.on('error', console.error.bind(console, 'Mongoose: Connection Error:'));
-// db.once('open', function () {
-//     console.log('Mongoose: Connection successful');
-// });
+var requestHandler = function (req,res,err) { 
 
-// //connect to the database
-// var db_status = mongoose.connect(config.getDbConnectionString(), {
-//     useMongoClient: true
-// });
+    console.log('recieved client...'); 
 
-//set view engine to ejs but parse plain html  
-// app.engine('html', ejs.renderFile);
-// app.set('view engine', 'html');
+    fs.readFile('views/b.html', function(err,page) {
+        if (err) { 
+            console.log('error readin html'); 
+            res.writeHeader(500, {'Content-Type': 'text/html '}); 
+            res.end('html error');
+            throw(err); 
+        } else {
+            res.writeHeader(200, {'Content-Type': 'text/html'}); 
+            res.write(page); 
+            res.end(); 
+        }
+    })
+}
+var app = http.createServer(requestHandler); 
 
-//static asset requests to public
-//app.use('/assets', express.static(path.join(__dirname, 'public')));
+app.listen(port, (err) => { 
+    if (err) { 
+        console.log('something happened lol'); 
+    }
 
-//use middleware 
-// app.use(bp.json());
-// app.use(bp.urlencoded({ extended: true }));
-
-app.get('/',function(req,res,err) { 
-    res.sendFile(path.join(__dirname,'./views/b.html')); 
-})
-
-//start server 
-app.listen(port); 
+    console.log('server listening...');
+    
+});
